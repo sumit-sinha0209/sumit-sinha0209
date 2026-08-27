edc9f25133e020c058c675707c51d9d918764f58
Sumit Sinha <sumit.sinha0209@gmail.com>
2026-08-27 19:05:37 +0530

    Update guamp_crave_los21.sh

--- guamp_crave_los21.sh
+++ guamp_crave_los21.sh
@@ -8,4 +8,13 @@ git clone https://github.com/sumit-sinha0209/local_manifests -b guamp_Lineage-21
 source build/envsetup.sh && \
 lunch lineage_guamp-ap2a-userdebug && \
 # make installclean && \
+
+if [ -n "$SELF_NAME" ]; then
+  git clone "https://$SELF_NAME@://github.com/sumit-sinha0209/private" /device/motorola/guamp/keys/
+else
+  echo "FALLBACK 10120988272848248236874"
+  export BOARD_AVB_KEY_PATH="external/avb/test/data/testkey_rsa2048.pem"
+  export PRODUCT_DEFAULT_DEV_CERTIFICATE="build/target/product/security/testkey"
+fi
+
 mka bacon
