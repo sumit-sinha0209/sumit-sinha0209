75282da6dc949562a35f85671c20b016c8fcd980
Sumit Sinha <sumit.sinha0209@gmail.com>
2026-08-30 12:33:58 +0530

    Update guamp_crave_los21.sh

--- guamp_crave_los21.sh
+++ guamp_crave_los21.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests && \
-git clone https://github.com/sumit-sinha0209/local_manifests -b guamp_Lineage-21 --depth 1 .repo/local_manifests && \
-/opt/crave/resync.sh && \
-source build/envsetup.sh && \
-lunch lineage_guamp-ap2a-userdebug && \
-mka bacon && \
-tar -czf ccache.tar.gz $CCACHE_DIR || true
+# rm -rf .repo/local_manifests && \
+# git clone https://github.com/sumit-sinha0209/local_manifests -b guamp_Lineage-21 --depth 1 .repo/local_manifests && \
+# /opt/crave/resync.sh && \
+# source build/envsetup.sh && \
+# lunch lineage_guamp-ap2a-userdebug && \
+# mka bacon && \
+tar -czf ./ccache.tar.gz $CCACHE_DIR
