ebcc4f4462731be06df8b96c0b3553ef65fd9070
Sumit Sinha <sumit.sinha0209@gmail.com>
2026-09-06 14:14:24 +0530

    Update guamp_crave_cr14.sh

--- guamp_crave_cr14.sh
+++ guamp_crave_cr14.sh
@@ -5,5 +5,5 @@ repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --depth="1" -
 git clone https://github.com/sumit-sinha0209/local_manifests -b guamp_crdroid-14 --depth 1 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 source build/envsetup.sh && \
-lunch crdroid_guamp-ap2a-userdebug && \
+lunch lineage_guamp-ap2a-userdebug && \
 brunch guamp
